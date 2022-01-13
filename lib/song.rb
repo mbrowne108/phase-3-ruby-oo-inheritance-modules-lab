4392672ca7efec7afee1f354ac352ff579ef1d19
Matt <mbrowne108@gmail.com>
2022-01-13 13:49:11 -0500

    commit tree 67cbce9ae1a19c9e943791569e63a5b40238ed26

--- lib/song.rb
+++ lib/song.rb
@@ -4,10 +4,15 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
+
   @@songs = []
 
   def initialize
-    @@songs << self
+    self.class.all << self
   end
 
   def self.find_by_name(name)
@@ -18,19 +23,7 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def artist=(artist)
     @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
